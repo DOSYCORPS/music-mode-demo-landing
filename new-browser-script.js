8242e1990e75b1a03204f79eef4dc5763ae1ebda
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2022-02-09 17:56:45 +0800

    "OK"

--- new-browser-script.js
+++ new-browser-script.js
@@ -5,7 +5,6 @@
     const TokenURL = () => new URL(`${API}/token`);
     const PayURL = () => new URL(`${API}/paid`);
     const StartURL = () => new URL(`${API}/start`);
-    const loadingUrl = `/loading.html`;
     const Steps = [
       {
         url: TokenURL,
@@ -41,8 +40,6 @@
     let locking = false;
 
     self.loadBrowser = loadBrowser;
-
-    installCopySharingLink(document);
 
     async function loadBrowser(event) {
       event.preventDefault();
@@ -82,46 +79,6 @@
       );
 
       return false;
-    }
-
-    function installCopySharingLink(doc) {
-      const target = doc.documentElement || doc.body;
-      console.log(target);
-      target.addEventListener('dblclick', copyShareLink);
-      target.addEventListener('contextmenu', copyShareLink);
-    }
-
-    async function copyShareLink(e) {
-      console.log(e);
-      const frameSrc = frame.src;
-      if ( ! frameSrc ) {
-        await delaySay(`Sorry, you haven't started a session yet. Click the 'Try right now' button, to start a browser session. Then you can double-tap or long-hold anywhere on the top of the page to share it!`);
-        return;
-      }
-      let shareWorked = false;
-      try {
-        const shareResult = await navigator.share({
-          title: 'Viewfinder Co-browsing session',
-          text: 'I invite you to join my co-browsing session.',
-          url: frameSrc
-        });
-        shareWorked = true;
-      } catch(e) {
-        await delaySay(`Oops, we couldn't share that link to another app. But let's try to copy it to the clipboard.`);
-      }
-      if ( shareWorked ) return;
-
-      let copyWorked = false;
-      try {
-        const copyResult = await navigator.clipboard.writeText(frameSrc); 
-        copyWorked = true;
-      } catch(e) {
-        await delaySay(`Sorry, copy to clipboard didn't work. But here's the cobrowsing sharing link: ${frameSrc}`);
-      }
-      if ( copyWorked ) {
-        await delaySay(`Success! The cobrowsing share link was copied to your clipboard. Send it to someone to invite them onto your session.`);
-      }
-      return;
     }
 
     async function delaySay(msg) {
